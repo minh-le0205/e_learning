e6e8c2e7c4d7ae35c4a4316cd842e821875ae746
minh-le0205 <minh.levunguyen@gmail.com>
2025-05-06 22:02:48 +0700

    Initial admin login function

--- routes/admin.php
+++ routes/admin.php
@@ -11,7 +11,7 @@ use App\Http\Controllers\Admin\Auth\RegisteredUserController;
 use App\Http\Controllers\Admin\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['middlewware' => "guest", "prefix" => "admin", "as" => "admin."], function () {
+Route::group(['middleware' => "guest", "prefix" => "admin", "as" => "admin."], function () {
     Route::get('register', [RegisteredUserController::class, 'create'])
         ->name('register');
 
@@ -20,7 +20,7 @@ Route::group(['middlewware' => "guest", "prefix" => "admin", "as" => "admin."], 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
         ->name('login');
 
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login.store');
 
     Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
         ->name('password.request');
